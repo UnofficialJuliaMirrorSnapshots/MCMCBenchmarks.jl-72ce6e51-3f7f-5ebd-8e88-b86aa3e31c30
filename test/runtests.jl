711932e02ab3fea6214c77d0248a2d9990c8ef4d
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-07-19 05:58:31 +0000

    Snapshot of branch master taken on 2019-07-19T01:58:29.713-04:00 from "https://github.com/UnofficialJuliaMirror/MCMCBenchmarks.jl-72ce6e51-3f7f-5ebd-8e88-b86aa3e31c30"

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,11 @@
 using MCMCBenchmarks,Test
 tests = [
 "generative_model_tests",
-"Gaussian_Tests"
+"Gaussian_Tests",
+"SDT_Tests",
+"LBA_Tests",
+"Poisson_Test",
+"Regression_Tests"
 ]
 
 res = map(tests) do t
